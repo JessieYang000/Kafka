226763d94cfc3c89d7c02c3ece87cebbe9ddebf7
JessieYang000 <yang.jieq@northeastern.edu>
2024-05-03 06:37:18 -0400

    set the outline of  the producer demo

--- kafka-beginners-course/kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
+++ kafka-beginners-course/kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
@@ -3,5 +3,14 @@ package io.conduktor.demos.kafka;
 public class ProducerDemo {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        //create Producer Properties
+
+        //create the Producer
+
+        //send data
+
+        //flush and close the producer
+
     }
 }
