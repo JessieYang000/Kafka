8640ffbb6dd05a0e69721a701924de16b576ff95
JessieYang000 <yang.jieq@northeastern.edu>
2024-05-03 09:26:33 -0400

    set properties to connect to Conduktor playground

--- kafka-beginners-course/kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
+++ kafka-beginners-course/kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
@@ -1,10 +1,28 @@
 package io.conduktor.demos.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
 public class ProducerDemo {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        log.info("Hello world!");
 
         //create Producer Properties
+        Properties properties = new Properties();
+
+        //connect to local host
+//        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+
+        //connect to Conduktor playground
+        properties.setProperty("bootstrap.servers", "enabling-bedbug-5399-us1-kafka.upstash.io:9092");
+        properties.setProperty("sasl.mechanism", "SCRAM-SHA-256");
+        properties.setProperty("security.protocol", "SASL_SSL");
+        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.scram.ScramLoginModule required");
+        properties.setProperty("username", "\"ZW5hYmxpbmctYmVkYnVnLTUzOTkkIQFZV2j4jzjVMilYT-sue6b4KOx-iN69jQs\"");
+        properties.setProperty("password", "\"OGFlNTFhYmMtNjM2NS00NWQ1LWFlMGUtMTYzZjI5OTBkZGFk\"");
 
         //create the Producer
 
