ac102d104a06874c4fdbc11c2bce40fbf3005182
JessieYang000 <yang.jieq@northeastern.edu>
2024-05-05 06:19:51 -0400

    move the setup of kafka properties to a separate file and add its path to load it in the producer

--- kafka-beginners-course/kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
+++ kafka-beginners-course/kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
@@ -6,29 +6,28 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class ProducerDemo {
     private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
     public static void main(String[] args) {
         log.info("Hello world!");
+//        System.out.println("Current working directory: " + System.getProperty("user.dir"));
 
         //create Producer Properties
         Properties properties = new Properties();
+        String path = "config.properties";// Relative path to config.properties from the current Java file
+        try (InputStream input = new FileInputStream(path)) {
+            properties.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
 
         //connect to local host
 //        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
-
-        //connect to Conduktor playground
-        properties.setProperty("bootstrap.servers", "enabling-bedbug-5399-us1-kafka.upstash.io:9092");
-        properties.setProperty("sasl.mechanism", "SCRAM-SHA-256");
-        properties.setProperty("security.protocol", "SASL_SSL");
-        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.scram.ScramLoginModule required " +
-                "username=\"ZW5hYmxpbmctYmVkYnVnLTUzOTkkIQFZV2j4jzjVMilYT-sue6b4KOx-iN69jQs\" " +
-                "password=\"OGFlNTFhYmMtNjM2NS00NWQ1LWFlMGUtMTYzZjI5OTBkZGFk\";");
-
-//        properties.setProperty("username", "\"ZW5hYmxpbmctYmVkYnVnLTUzOTkkIQFZV2j4jzjVMilYT-sue6b4KOx-iN69jQs\"");
-//        properties.setProperty("password", "\"OGFlNTFhYmMtNjM2NS00NWQ1LWFlMGUtMTYzZjI5OTBkZGFk\"");
 
         //set properties for producer
         properties.setProperty("key.serializer", StringSerializer.class.getName());
