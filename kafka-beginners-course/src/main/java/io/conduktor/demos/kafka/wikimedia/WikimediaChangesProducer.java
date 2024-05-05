4902ff2c3d690f77eb36f72a07f0177abbc6e5eb
JessieYang000 <yang.jieq@northeastern.edu>
2024-05-05 08:48:21 -0400

    add event handling

--- kafka-beginners-course/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-beginners-course/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
@@ -1,7 +1,51 @@
 package io.conduktor.demos.kafka.wikimedia;
 
+import com.launchdarkly.eventsource.EventSource;
+import com.launchdarkly.eventsource.StreamException;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.beans.EventHandler;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.util.Properties;
+
 public class WikimediaChangesProducer {
     public static void main(String[] args) {
+        //create Producer Properties
+        Properties properties = new Properties();
+        String path = "config.properties";// path to config.properties from the current working directory
+        try (InputStream input = new FileInputStream(path)) {
+            properties.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        //set properties for producer
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        //create kafka producer and producer record
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        String topic = "wikimedia.recentchange";
+
+        EventHandler eventHandler = TODO;
+
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
+        EventSource eventSource = builder.build();
+
+        //start the producer in another thread
+        try {
+            eventSource.start();
+        } catch (StreamException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
 }
