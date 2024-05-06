877b8a78a969f13c10c171d1fdf96fd0177b52ec
JessieYang000 <yang.jieq@northeastern.edu>
2024-05-06 10:38:58 -0400

    Open search consumer outline

--- kafka-beginners-course/kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/opensearch/OpenSearchConsumer.java
+++ kafka-beginners-course/kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/opensearch/OpenSearchConsumer.java
@@ -2,6 +2,12 @@ package io.conduktor.demos.kafka.opensearch;
 
 public class OpenSearchConsumer {
     public static void main(String[] args) {
+        //create the Open Search client
 
+        //create the Kafka client
+
+        //main code logic
+
+        //close things
     }
 }
